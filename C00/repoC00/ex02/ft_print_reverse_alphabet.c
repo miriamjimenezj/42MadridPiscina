3a88a8ece9fb9bbafd6bba60c5fdc3ece08c6ccf
miriamjimenezj <132669780+miriamjimenezj@users.noreply.github.com>
2023-07-07 13:56:54 +0200

    C00
    hasta ex04

---
+++ C00/repoC00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirjimen <mirjimen@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 12:04:28 by mirjimen          #+#    #+#             */
+/*   Updated: 2023/07/07 12:28:59 by mirjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+}
